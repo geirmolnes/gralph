c5bec6bf7dff26f7bbd444492cbff2279f3c48fa
Geir Molnes <g.molnes@gmail.com>
2026-02-13 11:59:08 +0100

    Bump version to 1.0.0

--- gralph/__init__.py
+++ gralph/__init__.py
@@ -4,5 +4,5 @@ gralph - Geir's Ralph Loop Scaffolding Tool
 Sets up and manages autonomous coding sessions with sandboxed execution.
 """
 
-__version__ = "0.9.0"
+__version__ = "1.0.0"
 GRALPH_DIR = ".gralph_planning"
